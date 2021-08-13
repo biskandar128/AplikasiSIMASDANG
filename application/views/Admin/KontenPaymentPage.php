4a9c2d75b8b398475b86559397aa56d671f55cfb
biskandar128 <biskandar128@gmail.com>
2021-08-13 20:23:32 +0700

    Aplikasi SIMASDANG(CI)

--- application/views/Admin/KontenPaymentPage.php
+++ application/views/Admin/KontenPaymentPage.php
@@ -116,7 +116,7 @@
 <!-- MODAL FORM UBAH -->
 <!-- ========================================================================================= -->
 <div class="modal fade" id="modalUbahPayment" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Form Ubah Konten</h5>
--- application/views/Admin/TransactionPage.php
+++ application/views/Admin/TransactionPage.php
@@ -156,7 +156,7 @@
 <!-- MODAL FORM UPDATE STATUS -->
 <!-- ========================================================================================= -->
 <div class="modal fade" id="modalTransactionStatus" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Update Status</h5>
